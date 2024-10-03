1732bd80d0c7a44cadbc50d11ae8eff21e9e8082
Ai_msm <soomin8442@gmail.com>
2024-10-03 12:52:19 +0900

    update/BOJ_fibo

---
+++ 백준/[BOJ]1003.py
@@ -0,0 +1,14 @@
+def fibo(n):
+    zero = [1,0,1]
+    one = [0,1,1]
+    if n >= 3:
+        for i in range(2,n):
+            zero.append(zero[i-1] + zero[i])
+            one.append(one[i-1]+one[i])
+    print(f'{zero[n]} {one[n]}')
+
+    
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    fibo(N)
