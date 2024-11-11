dc83186e0754435b133b1bdc9a43771072d27c85
SMA <sma002452@gmail.com>
2024-11-12 01:57:24 +0900

    1005번: ACM Craft

---
+++ 1005_ACM Craft.py
@@ -0,0 +1,45 @@
+# problem link: https://www.acmicpc.net/problem/1005
+# BFS? https://gmlwjd9405.github.io/2018/08/15/algorithm-bfs.html
+# DFS? https://gmlwjd9405.github.io/2018/08/14/algorithm-dfs.html
+# answer: https://velog.io/@enchantee/%EB%B0%B1%EC%A4%80-1005-Python-kvth6jbu
+
+import sys
+from collections import deque
+ 
+T=int(sys.stdin.readline())
+ 
+for _ in range(T):
+    N,K=map(int,sys.stdin.readline().split())#건물수, 건설순서규칙
+    time=[0]+list(map(int,sys.stdin.readline().split()))#건물들의 건설시간
+    print('time = ', time)
+    seq=[[] for _ in range(N+1)]#건설순서규칙
+    print('seq = ', seq)
+    inDegree=[0 for _ in range(N+1)]#진입차수
+    print('inDegree = ', inDegree)
+    DP=[0 for _ in range(N+1)]#해당 건물까지 걸리는 시간
+    print('DP = ', DP)
+ 
+    for _ in range(K):#건설순서규칙 저장
+        a,b=map(int,sys.stdin.readline().split())
+        seq[a].append(b)
+        print(seq)
+        inDegree[b]+=1
+        print(inDegree)
+ 
+    q = deque()
+    for i in range(1,N+1):#진입차수 0인거 찾아서 큐에 넣기
+        if inDegree[i]==0:
+            q.append(i)
+            DP[i]=time[i]
+ 
+    while q:
+        a=q.popleft()
+        for i in seq[a]:
+            inDegree[i]-=1#진입차수 줄이고
+            DP[i]=max(DP[a]+time[i],DP[i])#DP를 이용해 건설비용 갱신
+            if inDegree[i]==0:
+                q.append(i)
+ 
+ 
+    ans=int(sys.stdin.readline())
+    print(DP[ans])
